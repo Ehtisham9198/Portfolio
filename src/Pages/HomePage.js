8f59c1f39be2bae4473a55d9920b596dabd6ae3a
ehtisham mohd <125719040+Ehtisham9198@users.noreply.github.com>
2023-12-31 11:58:12 +0530

    first push

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,27 @@
+import React from "react";
+import classes from './HomePage.module.css';
+import pic from './myphoto.jpg'
+
+const HomePage=(props)=>{
+
+    return(
+        <div>
+            <header>
+            <span className={classes.Portfolio}> <h3><span className={classes.portfolio}> Ehtisham's</span> Portfolio </h3></span>
+            <div  className={classes.HeaderButtons}>
+            <button>Home</button>
+            <button>About me</button>
+            <button>Projects</button>
+            <button>Testimonials</button>
+            <button>Contacts</button>
+            </div>
+            </header>
+            <h1 className={classes.welcome}> <span className={classes.wel}>Welcome</span> <span className={classes.to}> to My Website </span></h1>
+            <h1 className={classes.name}>Hi, I'm <span className={classes.exactName}>Ehtisham</span><br /> a Software Developer</h1>
+            <p className={classes.pahra}>I'm a passionate and skilled Software Developer. I have good experience of both Frontend and Backend development</p> 
+            <img src={pic} alt="my photo" className={classes.pic} />
+        </div>
+    );
+}
+
+export default HomePage;
